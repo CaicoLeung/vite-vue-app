cc39871ae5e2156cc251d813c999d01d0b568d7c
CaicoLeung <lkk1053190902@gmail.com>
2021-05-10 18:04:23 +0800

    feat: app.vue改由tsx实现

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld';
+import logo from './assets/logo.png'
+import './app.less';
+
+export default defineComponent({
+  name: 'App',
+  render() {
+    return (
+      <>
+        <img alt="Vue logo" src={logo} />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"/>
+      </>
+    )
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app'
 import { message } from 'ant-design-vue';
 
 const app = createApp(App)
